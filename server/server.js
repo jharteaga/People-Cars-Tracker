60af8eabacf67fae6c0a9ad93f9768987634b7db
Jose Arteaga <jh.arteaga09@gmail.com>
2022-02-20 16:30:43 -0800

    init: initial workflow set, and server boilerplate

---
+++ server/server.js
@@ -0,0 +1,4 @@
+const express = require('express')
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import http from 'http'
