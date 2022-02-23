a9c0e6930b9e3b015739c8c1b9485bc143297e11
Jose Arteaga <jh.arteaga09@gmail.com>
2022-02-22 19:03:58 -0800

    feat: add antd modal for delete confirmation

--- client/src/components/buttons/RemoveCar.jsx
+++ client/src/components/buttons/RemoveCar.jsx
@@ -1,8 +1,12 @@
+import React, { useState } from 'react'
+import { Modal } from 'antd'
 import { DeleteOutlined } from '@ant-design/icons'
 import { useMutation } from '@apollo/client'
 import { GET_CARS, REMOVE_CAR } from '../../queries'
 
 const RemoveCar = ({ data }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
   const [removeCar] = useMutation(REMOVE_CAR, {
     update(proxy, { data: { removeCar } }) {
       const { cars } = proxy.readQuery({ query: GET_CARS })
@@ -15,31 +19,48 @@ const RemoveCar = ({ data }) => {
     }
   })
 
+  const handleConfirmation = () => {
+    handleRemove(data)
+    setIsModalVisible(false)
+  }
+
   const handleRemove = ({ id, ...rest }) => {
-    let result = window.confirm('Are you sure you want to delete this car?')
-    if (result) {
-      removeCar({
-        variables: {
-          id
-        },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          removeCar: {
-            __typename: 'Car',
-            id,
-            ...rest
-          }
+    removeCar({
+      variables: {
+        id
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        removeCar: {
+          __typename: 'Car',
+          id,
+          ...rest
         }
-      })
-    }
+      }
+    })
   }
 
   return (
-    <DeleteOutlined
-      key="delete"
-      style={{ color: '#f00' }}
-      onClick={() => handleRemove(data)}
-    />
+    <>
+      <DeleteOutlined
+        key="delete"
+        style={{ color: '#f00' }}
+        onClick={() => setIsModalVisible(true)}
+      />
+      <Modal
+        title="Remove car"
+        visible={isModalVisible}
+        onOk={handleConfirmation}
+        onCancel={() => setIsModalVisible(false)}
+      >
+        <p>
+          Are you sure you want to remove the car:{' '}
+          <strong>
+            {data.make} {data.model}
+          </strong>
+        </p>
+      </Modal>
+    </>
   )
 }
 
--- client/src/components/buttons/RemovePerson.jsx
+++ client/src/components/buttons/RemovePerson.jsx
@@ -1,8 +1,12 @@
+import React, { useState } from 'react'
+import { Modal } from 'antd'
 import { DeleteOutlined } from '@ant-design/icons'
 import { useMutation } from '@apollo/client'
 import { GET_PEOPLE, REMOVE_PERSON } from '../../queries'
 
 const RemovePerson = ({ data }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
   const [removePerson] = useMutation(REMOVE_PERSON, {
     update(proxy, { data: { removePerson } }) {
       const { people } = proxy.readQuery({ query: GET_PEOPLE })
@@ -15,31 +19,51 @@ const RemovePerson = ({ data }) => {
     }
   })
 
+  const handleConfirmation = () => {
+    handleRemove(data)
+    setIsModalVisible(false)
+  }
+
   const handleRemove = ({ id, ...rest }) => {
-    let result = window.confirm('Are you sure you want to delete this person?')
-    if (result) {
-      removePerson({
-        variables: {
-          id
-        },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          removePerson: {
-            __typename: 'Person',
-            id,
-            ...rest
-          }
+    // let result = window.confirm('Are you sure you want to delete this person?')
+    // if (result) {
+    removePerson({
+      variables: {
+        id
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        removePerson: {
+          __typename: 'Person',
+          id,
+          ...rest
         }
-      })
-    }
+      }
+    })
+    // }
   }
 
   return (
-    <DeleteOutlined
-      key="remove"
-      style={{ color: '#f00' }}
-      onClick={() => handleRemove(data)}
-    />
+    <>
+      <DeleteOutlined
+        key="remove"
+        style={{ color: '#f00' }}
+        onClick={() => setIsModalVisible(true)}
+      />
+      <Modal
+        title="Remove person"
+        visible={isModalVisible}
+        onOk={handleConfirmation}
+        onCancel={() => setIsModalVisible(false)}
+      >
+        <p>
+          Are you sure you want to remove the person:{' '}
+          <strong>
+            {data.firstName} {data.lastName}
+          </strong>
+        </p>
+      </Modal>
+    </>
   )
 }
 
